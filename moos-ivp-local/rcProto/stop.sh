92d720511fd2595fc9ba77f01d3161ff4e2d23aa
COAST <hyeminyoo@umail.ucsb.edu>
2018-05-27 15:19:18 -0700

    new branch

--- moos-ivp-local/rcProto/stop.sh
+++
@@ -1,5 +0,0 @@
-APPS="pAntler pHelmIvP pMarinePID MOOSDB iMOOS2Serial pEchoVar iGPSd pNodeReporter pLogger"
-
-printf "Killing all processes ... \n"
-killall -9 ${APPS}      
-printf "Done killing processes.   \n"
