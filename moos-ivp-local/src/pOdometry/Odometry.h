8c4fee7a3785094dd59b26b3407f45b4bcae2d8b
COAST <hyeminyoo@umail.ucsb.edu>
2018-05-27 15:26:28 -0700

    pOdometry files

---
+++ moos-ivp-local/src/pOdometry/Odometry.h
@@ -0,0 +1,41 @@
+/************************************************************/
+/*    NAME: Min Dai                                              */
+/*    ORGN: MIT                                             */
+/*    FILE: Odometry.h                                          */
+/*    DATE:                                                 */
+/************************************************************/
+
+#ifndef Odometry_HEADER
+#define Odometry_HEADER
+
+#include "MOOS/libMOOS/MOOSLib.h"
+
+class Odometry : public CMOOSApp
+{
+ public:
+   Odometry();
+   ~Odometry();
+
+ protected: // Standard MOOSApp functions to overload
+   bool OnNewMail(MOOSMSG_LIST &NewMail);
+   bool Iterate();
+   bool OnConnectToServer();
+   bool OnStartUp();
+
+ protected:
+ void RegisterVariables();
+//added
+
+ bool   m_first_reading;
+ double m_current_x;
+ double m_current_y;
+ double m_previous_x;
+ double m_previous_y;
+ double m_total_distance;
+
+ private: // Configuration variables
+
+ private: // State variables
+};
+
+#endif
